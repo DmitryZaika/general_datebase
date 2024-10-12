f59566b3351b6e74733d76ec8aac39ba5dc210e7
Dmitry Zaika <majukita777@gmail.com>
2024-10-12 07:23:03 -0400

    styled header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -7,22 +7,44 @@ interface LinkProps {
 
 function LinkCard({ href, children }: LinkProps) {
   return (
-    <li className="flex items-center justify-center text-yellow-400  bg-[#333] rounded-[]">
-      <Link to={href}>{children}</Link>
+    <li className="flex items-center justify-center ">
+      <Link
+        className="
+      border-2 w-[90%] 
+      text-center 
+      md:px-[10px]
+      py-[7px]
+      border-solid
+      border-[#ffd700]
+      bg-[#333]
+      text-[#ffd700]
+      rounded-[10px]
+      font-bold
+      transition-colors
+      duration-300
+      ease-in-out
+      md:w-[100%]
+     hover:bg-[#ffd700]
+    hover:text-gray-800
+ "
+        to={href}
+      >
+        {children}
+      </Link>
     </li>
   );
 }
 
 export function Header() {
   return (
-    <header className="flex content-center flex-col align-middle gap-5">
+    <header className="flex content-center flex-col align-middle gap-5 m-10 md:flex-row">
       <div className="logo ">
-        <a className="flex justify-center" href="index.html">
+        <a className="flex justify-center" href="/">
           <img src="./images/logo_gd_main.webp" alt="Logo" />
         </a>
       </div>
-      <nav className="main-nav">
-        <ul className="flex gap-10 h-full flex-col">
+      <nav className="w-full">
+        <ul className="flex gap-5 h-full flex-col md:flex-row md:justify-center md:items-center ">
           <LinkCard href="/">Database</LinkCard>
           <LinkCard href="/instructions">Instructions</LinkCard>
           <LinkCard href="/special-order">Special Order</LinkCard>
