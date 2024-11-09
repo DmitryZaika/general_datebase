9860ef28a898075250cb77f408cb6f893fbca21c
Dmitry Zaika <majukita777@gmail.com>
2024-11-08 20:09:30 -0500

    update admin.edit

--- app/routes/admin.stones.edit.$stone.tsx
+++ app/routes/admin.stones.edit.$stone.tsx
@@ -1,23 +1,14 @@
-import { zodResolver } from "@hookform/resolvers/zod";
 import {
   ActionFunctionArgs,
   json,
   LoaderFunctionArgs,
   redirect,
 } from "@remix-run/node";
-import {
-  useSubmit,
-  Form,
-  useNavigate,
-  useLoaderData,
-  useNavigation,
-} from "@remix-run/react";
+import { useNavigate, useLoaderData, useNavigation } from "@remix-run/react";
 import { FormField } from "../components/ui/form";
-import { getValidatedFormData } from "remix-hook-form";
 import { z } from "zod";
 import { InputItem } from "~/components/molecules/InputItem";
 
-import { useForm } from "react-hook-form";
 import {
   Dialog,
   DialogContent,
@@ -34,33 +25,37 @@ import { toastData } from "~/utils/toastHelpers";
 import { MultiPartForm } from "~/components/molecules/MultiPartForm";
 import { FileInput } from "~/components/molecules/FileInput";
 import { LoadingButton } from "~/components/molecules/LoadingButton";
+import { parseMutliForm } from "~/utils/parseMultiForm";
+import { useCustomOptionalForm } from "~/utils/useCustomForm";
 
 const stoneSchema = z.object({
   name: z.string().min(1),
   type: z.enum(["granite", "quartz", "marble", "dolomite", "quartzite"]),
-  file: z.any(),
 });
-
-type FormData = z.infer<typeof stoneSchema>;
-
-const resolver = zodResolver(stoneSchema);
 
 export async function action({ request, params }: ActionFunctionArgs) {
   const stoneId = params.stone;
-  const {
-    errors,
-    data,
-    receivedValues: defaultValues,
-  } = await getValidatedFormData<FormData>(request, resolver);
-  if (errors) {
-    return json({ errors, defaultValues });
+  const { errors, data } = await parseMutliForm(request, stoneSchema, "stones");
+  if (errors || !data) {
+    return json({ errors });
   }
 
   try {
-    const result = await db.execute(
-      `UPDATE main.stones SET name = ?, type = ?, url = ? WHERE id = ?`,
-      [data.name, data.type, data.file, stoneId]
-    );
+    let result;
+    console.log(typeof data.file);
+
+    if (data.file && data.file !== "undefined") {
+      result = await db.execute(
+        `UPDATE main.stones SET name = ?, type = ?, url = ? WHERE id = ?`,
+        [data.name, data.type, data.file, stoneId]
+      );
+    } else {
+      result = await db.execute(
+        `UPDATE main.stones SET name = ?, type = ? WHERE id = ?`,
+        [data.name, data.type, stoneId]
+      );
+    }
+
     console.log(result);
   } catch (error) {
     console.error("Error connecting to the database: ", errors);
@@ -74,11 +69,11 @@ export async function action({ request, params }: ActionFunctionArgs) {
 
 export const loader = async ({ params }: LoaderFunctionArgs) => {
   if (params.stone === undefined) {
-    return json({ name: undefined, type: undefined, file: undefined });
+    return json({ name: undefined, type: undefined, url: undefined });
   }
   const stoneId = parseInt(params.stone);
 
-  const stone = await selectId<{ name: string; type: string; file: string }>(
+  const stone = await selectId<{ name: string; type: string; url: string }>(
     db,
     "select name, type, url from stones WHERE id = ?",
     stoneId
@@ -86,24 +81,25 @@ export const loader = async ({ params }: LoaderFunctionArgs) => {
   return json({
     name: stone?.name,
     type: stone?.type,
-    file: stone?.file,
+    url: stone?.url,
   });
 };
 
 export default function StonesEdit() {
   const navigate = useNavigate();
   const isSubmitting = useNavigation().state === "submitting";
-  const { name, type, file } = useLoaderData<typeof loader>();
-  const submit = useSubmit();
-  const form = useForm<FormData>({
-    resolver,
-    defaultValues: stoneSchema.parse({ name, type, file }),
-  });
+  const { name, type, url } = useLoaderData<typeof loader>();
+
+  const form = useCustomOptionalForm(
+    stoneSchema,
+    stoneSchema.parse({ name, type, url })
+  );
   const handleChange = (open: boolean) => {
     if (open === false) {
       navigate("..");
     }
   };
+
   return (
     <Dialog open={true} onOpenChange={handleChange}>
       <DialogContent className="sm:max-w-[425px]">
@@ -111,63 +107,50 @@ export default function StonesEdit() {
           <DialogTitle>Edit Stone</DialogTitle>
         </DialogHeader>
         <MultiPartForm form={form}>
-          <Form
-            id="customerForm"
-            method="post"
-            onSubmit={form.handleSubmit(
-              (data) => {
-                submit(data, {
-                  method: "post",
-                  encType: "multipart/form-data",
-                });
-              },
-              (errors) => console.log(errors)
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <InputItem
+                name="Name"
+                placeholder={"Name of the stone"}
+                field={field}
+              />
             )}
-          >
-            <FormField
-              control={form.control}
-              name="name"
-              render={({ field }) => (
-                <InputItem
-                  name="Name"
-                  placeholder={"Name of the stone"}
-                  field={field}
-                />
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="type"
-              render={({ field }) => (
-                <SelectInput
-                  field={field}
-                  placeholder="Type of the Stone"
-                  name="Type"
-                  options={[
-                    "Granite",
-                    "Quartz",
-                    "Marble",
-                    "Dolomite",
-                    "Quartzite",
-                  ]}
-                />
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="file"
-              render={({ field }) => (
-                <FileInput
-                  inputName="stones"
-                  id="image"
-                  onChange={field.onChange}
-                />
-              )}
-            />
-            <DialogFooter>
-              <LoadingButton loading={isSubmitting}>Edit Stone</LoadingButton>
-            </DialogFooter>
-          </Form>
+          />
+          <FormField
+            control={form.control}
+            name="type"
+            render={({ field }) => (
+              <SelectInput
+                field={field}
+                placeholder="Type of the Stone"
+                name="Type"
+                options={[
+                  "Granite",
+                  "Quartz",
+                  "Marble",
+                  "Dolomite",
+                  "Quartzite",
+                ]}
+              />
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="file"
+            render={({ field }) => (
+              <FileInput
+                inputName="stones"
+                id="image"
+                onChange={field.onChange}
+              />
+            )}
+          />
+          <p>{url}</p>
+          <DialogFooter>
+            <LoadingButton loading={isSubmitting}>Edit Stone</LoadingButton>
+          </DialogFooter>
         </MultiPartForm>
       </DialogContent>
     </Dialog>
