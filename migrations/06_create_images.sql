0e738f0b1b6c9a10fdea7883678d8c0ae10c70aa
Dmitry Zaika <majukita777@gmail.com>
2024-11-17 08:28:47 -0500

    added sql create tables

--- migrations/06_create_images.sql
+++ migrations/06_create_images.sql
@@ -1,6 +1,5 @@
-CREATE TABLE documents (
+CREATE TABLE images (
     id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(100),
-    src VARCHAR(255), 
     created_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
---
+++ migrations/08_create_supplier.sql
@@ -0,0 +1,10 @@
+CREATE TABLE suppliers (
+    id INT PRIMARY KEY,
+    website VARCHAR(255),
+    supplier_name VARCHAR(255),
+    manager VARCHAR(255),
+    phone VARCHAR(50),
+    email VARCHAR(255),
+    notes VARCHAR(255)
+    created_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
