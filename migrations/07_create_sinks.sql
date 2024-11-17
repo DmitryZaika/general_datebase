3adf7ca8e65965c9e62bb4ff385f85249da1306d
Dmitry Zaika <majukita777@gmail.com>
2024-11-17 08:28:47 -0500

    added sql create tables

--- migrations/07_create_sinks.sql
+++ migrations/07_create_sinks.sql
@@ -1,6 +1,5 @@
-CREATE TABLE documents (
+CREATE TABLE sinks (
     id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(100),
-    src VARCHAR(255), 
     created_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
