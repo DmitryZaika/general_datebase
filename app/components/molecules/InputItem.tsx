f90399dce6d762a5045270fd81ca7de2a406acec
Dmitry Zaika <majukita777@gmail.com>
2024-11-17 13:17:03 -0500

    changes

--- app/components/molecules/InputItem.tsx
+++ app/components/molecules/InputItem.tsx
@@ -5,16 +5,18 @@ export function InputItem({
   name,
   placeholder,
   field,
+  type,
 }: {
   name: string;
   placeholder: string;
   field: object;
+  type?: string;
 }) {
   return (
     <FormItem>
       <FormLabel>{name}</FormLabel>
       <FormControl>
-        <Input placeholder={placeholder} {...field} />
+        <Input type={type} placeholder={placeholder} {...field} />
       </FormControl>
       <FormMessage />
     </FormItem>
