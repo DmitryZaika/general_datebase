9e57886d4c8bf57abb7bb5f6b3b81d52afba1dab
Colin Delahunty <colin99delahunty@gmail.com>
2024-11-22 20:57:21 -0500

    feat: adding instructions

--- app/routes/admin.instructions.tsx
+++ app/routes/admin.instructions.tsx
@@ -1,11 +1,77 @@
-import { PageLayout } from "~/components/PageLayout";
+import { json, LoaderFunctionArgs, redirect } from "@remix-run/node";
+import { Link, Outlet, useLoaderData } from "@remix-run/react";
+import { Button } from "~/components/ui/button";
+import { TableBody, TableCaption, TableCell, TableFooter, TableHead, TableHeader, Table, TableRow } from "~/components/ui/table";
+import { db } from "~/db.server";
+import { selectMany } from "~/utils/queryHelpers";
+import { getAdminUser } from "~/utils/session.server";
+interface Instructions {
+  id: number;
+  title: string;
+  parent_id: number;
+  order: number;
+  rich_text: string;
+}
+
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+  let user;
+  try {
+    user = await getAdminUser(request);
+  } catch (error) {
+    return redirect(`/login?error=${error}`);
+  }
+  const instructions = await selectMany<Instructions>(
+    db,
+    "select id, title, parent_id, place, rich_text from instructions"
+  );
+  return json({ instructions, user });
+};
+
 
 export default function AdminInstructions() {
+  const { instructions } = useLoaderData<typeof loader>();
   return (
     <>
-      <PageLayout title="Admin Instructions">
-        <h1>Admin Instructions</h1>
-      </PageLayout>
-    </>
-  );
+    <Link to={`add`} relative="path">
+      <Button>Add</Button>
+    </Link>
+    <Table>
+      <TableCaption>A list of your Instructions</TableCaption>
+      <TableHeader>
+        <TableRow>
+          <TableHead className="text-xl w-[200px]">Title</TableHead>{" "}
+          <TableHead className="text-xl">Parent Id</TableHead>
+          <TableHead className="text-xl">Order</TableHead>
+          <TableHead className="text-xl text-right pr-4">
+            Edit Instruction
+          </TableHead>{" "}
+          <TableHead className="text-right text-xl">Delete Instruction</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {instructions.map((instruction) => (
+          <TableRow key={instruction.id}>
+            <TableCell className=" font-medium w-[200px]">
+              {instruction.title}
+            </TableCell>{" "}
+            <TableCell>{instruction.parent_id}</TableCell>
+            <TableCell>{instruction.order}</TableCell>
+            <TableCell className="text-right pr-4">
+              <Link to={`edit/${instruction.id}`} className="text-xl">
+                Edit
+              </Link>
+            </TableCell>
+            <TableCell className="text-right w-[200px]">
+              <Link to={`delete/${instruction.id}`} className="text-xl">
+                Delete
+              </Link>
+            </TableCell>
+            
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+    <Outlet />
+  </>
+)
 }
