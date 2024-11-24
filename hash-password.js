676eec31e4b25adaafd9ec39f770c641ad220fbc
Colin Delahunty <colin99delahunty@gmail.com>
2024-11-24 11:04:43 -0500

    Add password hasher

---
+++ hash-password.js
@@ -0,0 +1,20 @@
+import bcrypt from "bcryptjs";
+
+const args = process.argv.slice(2);
+
+if (args.length === 0) {
+  console.error("Usage: node hash-password.js <password>");
+  process.exit(1);
+}
+
+const password = args[0];
+
+(async () => {
+  try {
+    const passwordHash = await bcrypt.hash(password, 10);
+    console.log("Hashed Password:", passwordHash);
+  } catch (error) {
+    console.error("Error hashing password:", error);
+    process.exit(1);
+  }
+})();
