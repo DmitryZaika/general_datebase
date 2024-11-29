4d4acacc850ccbf8e76a0f413957f526490ae0c4
DmitryZaika <dmitryzaika9@gmail.com>
2024-11-28 21:23:48 -0500

    updated Chat function

--- app/components/atoms/ChatIcon.tsx
+++ app/components/atoms/ChatIcon.tsx
@@ -1,10 +1,32 @@
-import { MessageSquare } from 'lucide-react';
+import { MessageSquare } from "lucide-react";
 import clsx from "clsx";
 
-export function ChatIcon({ className, onClick }: { className?: string, onClick: () => void }) {
-    return (
-        <div className={clsx("bg-gray-500 rounded-full", className)} onClick={onClick}>
-        <MessageSquare size={40}/>
-        </div>
-    );
+export function ChatIcon({
+  className,
+  onClick,
+}: {
+  className?: string;
+  onClick: () => void;
+}) {
+  return (
+    <button
+      onClick={onClick}
+      className="fixed bottom-5 right-5 bg-blue-500 text-white p-4 rounded-full shadow-lg hover:bg-blue-600 transition"
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth="2"
+        stroke="currentColor"
+        className="w-6 h-6"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M12 20.25c4.97 0 9-3.813 9-8.504 0-4.692-4.03-8.496-9-8.496S3 7.054 3 11.746c0 1.846.728 3.559 1.938 4.875L3 20.25l5.455-2.224a10.5 10.5 0 003.545.624z"
+        />
+      </svg>
+    </button>
+  );
 }
--- app/routes/admin.instructions.delete.$instruction.tsx
+++ app/routes/admin.instructions.delete.$instruction.tsx
@@ -1,94 +1,94 @@
 import {
-    ActionFunctionArgs,
-    LoaderFunctionArgs,
-    redirect,
-  } from "@remix-run/node";
-  import { Form, useLoaderData, useNavigate } from "@remix-run/react";
-  import { selectId } from "~/utils/queryHelpers";
-  import { Button } from "~/components/ui/button";
-  
-  import {
-    Dialog,
-    DialogContent,
-    DialogDescription,
-    DialogFooter,
-    DialogHeader,
-    DialogTitle,
-  } from "~/components/ui/dialog";
-  
-  import { db } from "~/db.server";
-  import { commitSession, getSession } from "~/sessions";
-  import { forceRedirectError, toastData } from "~/utils/toastHelpers";
-  import { getAdminUser } from "~/utils/session.server";
-  
-  export async function action({ params, request }: ActionFunctionArgs) {
-    try {
-      await getAdminUser(request);
-    } catch (error) {
-      return redirect(`/login?error=${error}`);
-    }
-    const instructionId = params.instruction;
-    try {
-      const result = await db.execute(`DELETE FROM main.instructions WHERE id = ?`, [
-        instructionId,
-      ]);
-      console.log(result);
-    } catch (error) {
-      console.error("Error connecting to the database: ", error);
-    }
-    const session = await getSession(request.headers.get("Cookie"));
-    session.flash("message", toastData("Success", "instruction Deleted"));
-    return redirect("..", {
-      headers: { "Set-Cookie": await commitSession(session) },
-    });
+  ActionFunctionArgs,
+  LoaderFunctionArgs,
+  redirect,
+} from "@remix-run/node";
+import { Form, useLoaderData, useNavigate } from "@remix-run/react";
+import { selectId } from "~/utils/queryHelpers";
+import { Button } from "~/components/ui/button";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "~/components/ui/dialog";
+
+import { db } from "~/db.server";
+import { commitSession, getSession } from "~/sessions";
+import { forceRedirectError, toastData } from "~/utils/toastHelpers";
+import { getAdminUser } from "~/utils/session.server";
+
+export async function action({ params, request }: ActionFunctionArgs) {
+  try {
+    await getAdminUser(request);
+  } catch (error) {
+    return redirect(`/login?error=${error}`);
   }
-  
-  export const loader = async ({ params, request }: LoaderFunctionArgs) => {
-    try {
-      await getAdminUser(request);
-    } catch (error) {
-      return redirect(`/login?error=${error}`);
-    }
-    if (!params.instruction) {
-      return forceRedirectError(request.headers, "No instruction id provided");
-    }
-    const instructionId = parseInt(params.instruction);
-  
-    const instruction = await selectId<{ title: string }>(
-      db,
-      "select title from instructions WHERE id = ?",
-      instructionId
+  const instructionId = params.instruction;
+  try {
+    const result = await db.execute(
+      `DELETE FROM main.instructions WHERE id = ?`,
+      [instructionId]
     );
-    return {
-      title: instruction?.title
-    };
+    console.log(result);
+  } catch (error) {
+    console.error("Error connecting to the database: ", error);
+  }
+  const session = await getSession(request.headers.get("Cookie"));
+  session.flash("message", toastData("Success", "instruction Deleted"));
+  return redirect("..", {
+    headers: { "Set-Cookie": await commitSession(session) },
+  });
+}
+
+export const loader = async ({ params, request }: LoaderFunctionArgs) => {
+  try {
+    await getAdminUser(request);
+  } catch (error) {
+    return redirect(`/login?error=${error}`);
+  }
+  if (!params.instruction) {
+    return forceRedirectError(request.headers, "No instruction id provided");
+  }
+  const instructionId = parseInt(params.instruction);
+
+  const instruction = await selectId<{ title: string }>(
+    db,
+    "select title from instructions WHERE id = ?",
+    instructionId
+  );
+  return {
+    title: instruction?.title,
   };
-  
-  export default function InstructionsDelete() {
-    const navigate = useNavigate();
-    const { title } = useLoaderData<typeof loader>();
-  
-    const handleChange = (open: boolean) => {
-      if (open === false) {
-        navigate("..");
-      }
-    };
-    return (
-      <Dialog open={true} onOpenChange={handleChange}>
-        <DialogContent className="sm:max-w-[425px]">
-          <DialogHeader>
-            <DialogTitle>Delete Instruction</DialogTitle>
-            <DialogDescription>
-              Are you sure you want to delete {title}?
-            </DialogDescription>
-          </DialogHeader>
-          <Form id="customerForm" method="post">
-            <DialogFooter>
-              <Button type="submit">Delete Instruction</Button>
-            </DialogFooter>
-          </Form>
-        </DialogContent>
-      </Dialog>
-    );
-  }
-  
+};
+
+export default function InstructionsDelete() {
+  const navigate = useNavigate();
+  const { title } = useLoaderData<typeof loader>();
+
+  const handleChange = (open: boolean) => {
+    if (open === false) {
+      navigate("..");
+    }
+  };
+  return (
+    <Dialog open={true} onOpenChange={handleChange}>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Delete Instruction</DialogTitle>
+          <DialogDescription>
+            Are you sure you want to delete {title}?
+          </DialogDescription>
+        </DialogHeader>
+        <Form id="customerForm" method="post">
+          <DialogFooter>
+            <Button type="submit">Delete Instruction</Button>
+          </DialogFooter>
+        </Form>
+      </DialogContent>
+    </Dialog>
+  );
+}
