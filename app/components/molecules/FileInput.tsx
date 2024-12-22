403d5c43e621bd7c4d12fa404861577c33ea8cb4
DmitryZaika <dmitryzaika9@gmail.com>
2024-12-21 21:36:41 -0500

    updated size

--- app/components/molecules/FileInput.tsx
+++ app/components/molecules/FileInput.tsx
@@ -1,4 +1,4 @@
-import Compressor from 'compressorjs';
+import Compressor from "compressorjs";
 
 import { FormControl, FormItem, FormLabel, FormMessage } from "../ui/form";
 import { Input } from "~/components/ui/input";
@@ -11,21 +11,36 @@ type FileInput = {
 
 export function FileInput({ onChange, id }: FileInput) {
   function compressImage(file: File | undefined) {
-    if (!file) {
-      return;
-    }
-    console.log(file.size)
-    new Compressor(file, {
-      quality: 0.3,
+    if (!file) return;
 
-      success(result) {
-        onChange(result)
-      },
-      error(err) {
-        console.log(err.message);
-      },
-  });
+    const THREE_MB = 3 * 1024 * 1024;
+    const FIVE_HUNDRED_KB = 500 * 1024;
+
+    if (file.size > THREE_MB) {
+      new Compressor(file, {
+        quality: 0.2,
+        success(result) {
+          onChange(result);
+        },
+        error(err) {
+          console.error(err.message);
+        },
+      });
+    } else if (file.size > FIVE_HUNDRED_KB) {
+      new Compressor(file, {
+        quality: 0.3,
+        success(result) {
+          onChange(result);
+        },
+        error(err) {
+          console.error(err.message);
+        },
+      });
+    } else {
+      onChange(file);
+    }
   }
+
   return (
     <FormItem>
       <FormLabel>Image</FormLabel>
