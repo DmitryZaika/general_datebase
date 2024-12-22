7658ddfcab6a161d7fd43abfc132165d2b7e2c8b
DmitryZaika <dmitryzaika9@gmail.com>
2024-12-22 11:58:18 -0500

    updated document

--- app/routes/employee.documents.tsx
+++ app/routes/employee.documents.tsx
@@ -1,6 +1,8 @@
 import { LoaderFunctionArgs, redirect } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
 import { Document, Page, pdfjs } from "react-pdf";
+import "react-pdf/dist/esm/Page/AnnotationLayer.css";
+import "react-pdf/dist/esm/Page/TextLayer.css";
 import {
   Accordion,
   AccordionItem,
