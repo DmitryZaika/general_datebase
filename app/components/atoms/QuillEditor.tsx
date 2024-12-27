2a83993f27906a9a851d75ece2a9de0044c4e15c
Colin Delahunty <colin99delahunty@gmail.com>
2024-12-26 19:53:36 -0500

    feat: QuillEditor

---
+++ app/components/atoms/QuillEditor.tsx
@@ -0,0 +1,39 @@
+import { useEffect } from 'react';
+import { useQuill } from 'react-quilljs';
+
+import 'quill/dist/quill.snow.css';
+
+interface IQuillEditorProps {
+  value: string;
+  onChange: (value: string) => void;
+}
+
+export function QuillEditor({value, onChange}: IQuillEditorProps) {
+  const { quill, quillRef } = useQuill();
+
+  useEffect(() => {
+    if (quill) {
+      quill.clipboard.dangerouslyPasteHTML(value);
+    }
+  }, [quill]);
+
+  useEffect(() => {
+    if (quill) {
+      quill.on('text-change', (delta, oldDelta, source) => {
+        /*
+        console.log(quill.getText());
+        console.log(quill.getContents());
+        console.log(quill.root.innerHTML);
+        console.log(quillRef.current.firstChild.innerHTML);
+         */
+        onChange(quill.root.innerHTML);
+      });
+    }
+  }, [quill]);
+
+  return (
+    <div>
+      <div ref={quillRef} />
+    </div>
+  );
+};
---
+++ app/components/molecules/QuillInput.tsx
@@ -0,0 +1,20 @@
+import { FormControl, FormItem, FormLabel, FormMessage } from "../ui/form";
+import { QuillEditor } from "../atoms/QuillEditor";
+
+export function QuillInput({
+  name,
+  field,
+}: {
+  name: string;
+  field: object;
+}) {
+  return (
+    <FormItem>
+      <FormLabel>{name}</FormLabel>
+      <FormControl>
+        <QuillEditor {...field} />
+      </FormControl>
+      <FormMessage />
+    </FormItem>
+  );
+}
