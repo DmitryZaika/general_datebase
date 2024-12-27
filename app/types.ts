bdd6cfad2415ecfd61a983a6e6e05f246ed9fe19
DmitryZaika <dmitryzaika9@gmail.com>
2024-12-26 19:55:34 -0500

    feat: send context

--- app/types.ts
+++ app/types.ts
@@ -3,3 +3,15 @@ export interface Todo {
   name: string;
   is_done: boolean;
 }
+
+export interface InstructionSlim {
+  id: number;
+  title: string | null;
+  rich_text: string;
+}
+
+export interface Instruction extends InstructionSlim {
+  parent_id: number | null;
+  after_id: number | null;
+  children?: Instruction[];
+}
