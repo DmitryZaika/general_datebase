0c5a302aa2054d08b15a0f3c6985f7b0c90b2a88
DmitryZaika <dmitryzaika9@gmail.com>
2024-12-27 19:09:04 -0500

    updated instruction add and employee. instructions

--- app/components/molecules/QuillInput.tsx
+++ app/components/molecules/QuillInput.tsx
@@ -7,6 +7,7 @@ export function QuillInput({
 }: {
   name: string;
   field: object;
+  className?: string;
 }) {
   return (
     <FormItem>
--- app/routes/employee.instructions.tsx
+++ app/routes/employee.instructions.tsx
@@ -7,7 +7,6 @@ import {
   AccordionTrigger,
 } from "~/components/ui/accordion";
 import { db } from "~/db.server";
-
 import { selectMany } from "~/utils/queryHelpers";
 import { getEmployeeUser } from "~/utils/session.server";
 import { PageLayout } from "~/components/PageLayout";
@@ -39,14 +38,19 @@ export const loader = async ({ request }: LoaderFunctionArgs) => {
 };
 
 const InstructionItem: React.FC<InstructionItemProps> = ({ instruction }) => {
-  if (instruction.title) {
+  const hasTitle = Boolean(instruction.title);
+
+  if (hasTitle) {
     return (
       <AccordionItem value={instruction.id.toString()}>
         <AccordionTrigger className="font-medium text-xl underline underline-offset-4 py-4">
           {instruction.title}
         </AccordionTrigger>
         <AccordionContent>
-          <p className="text-black-500 pl-5">{instruction.text}</p>
+          <div
+            className="text-black-500 pl-5"
+            dangerouslySetInnerHTML={{ __html: instruction.text }}
+          />
           {instruction.children.length > 0 && (
             <Accordion type="multiple" className="ml-5">
               {instruction.children.map((childInstruction) => (
@@ -63,9 +67,10 @@ const InstructionItem: React.FC<InstructionItemProps> = ({ instruction }) => {
   } else {
     return (
       <div className="py-4">
-        <p className="text-black overflow-auto break-words w-full">
-          {instruction.text}
-        </p>
+        <div
+          className="text-black overflow-auto break-words w-full"
+          dangerouslySetInnerHTML={{ __html: instruction.text }}
+        />
         {instruction.children.length > 0 && (
           <div className="ml-5">
             {instruction.children.map((childInstruction) => (
@@ -81,61 +86,54 @@ const InstructionItem: React.FC<InstructionItemProps> = ({ instruction }) => {
   }
 };
 
+function cleanData(instructions: Instruction[]): InstructionNode[] {
+  const nodeMap: Map<number, InstructionNode> = new Map();
+  instructions.forEach((item) => {
+    nodeMap.set(item.id, {
+      id: item.id,
+      title: item.title,
+      text: item.rich_text,
+      after_id: item.after_id,
+      children: [],
+    });
+  });
+  const rootNodes: InstructionNode[] = [];
+  const insertNodeInOrder = (
+    nodes: InstructionNode[],
+    node: InstructionNode
+  ) => {
+    if (node.after_id === null) {
+      nodes.unshift(node);
+    } else {
+      const index = nodes.findIndex((n) => n.id === node.after_id);
+      if (index !== -1) {
+        nodes.splice(index + 1, 0, node);
+      } else {
+        nodes.push(node);
+      }
+    }
+  };
+  instructions.forEach((item) => {
+    const node = nodeMap.get(item.id)!;
+    if (item.parent_id === null) {
+      insertNodeInOrder(rootNodes, node);
+    } else {
+      const parentNode = nodeMap.get(item.parent_id);
+      if (parentNode) {
+        insertNodeInOrder(parentNode.children, node);
+      } else {
+        console.warn(
+          `Parent with id ${item.parent_id} not found for item id ${item.id}`
+        );
+      }
+    }
+  });
+  return rootNodes;
+}
+
 export default function Instructions() {
   const { instructions } = useLoaderData<typeof loader>();
-
-  function cleanData(instructions: Instruction[]): InstructionNode[] {
-    const nodeMap: Map<number, InstructionNode> = new Map();
-
-    instructions.forEach((item) => {
-      nodeMap.set(item.id, {
-        id: item.id,
-        title: item.title,
-        text: item.rich_text,
-        after_id: item.after_id,
-        children: [],
-      });
-    });
-
-    const rootNodes: InstructionNode[] = [];
-
-    const insertNodeInOrder = (
-      nodes: InstructionNode[],
-      node: InstructionNode
-    ) => {
-      if (node.after_id === null) {
-        nodes.unshift(node);
-      } else {
-        const index = nodes.findIndex((n) => n.id === node.after_id);
-        if (index !== -1) {
-          nodes.splice(index + 1, 0, node);
-        } else {
-          nodes.push(node);
-        }
-      }
-    };
-
-    instructions.forEach((item) => {
-      const node = nodeMap.get(item.id)!;
-      if (item.parent_id === null) {
-        insertNodeInOrder(rootNodes, node);
-      } else {
-        const parentNode = nodeMap.get(item.parent_id);
-        if (parentNode) {
-          insertNodeInOrder(parentNode.children, node);
-        } else {
-          console.warn(
-            `Parent with id ${item.parent_id} not found for item id ${item.id}`
-          );
-        }
-      }
-    });
-
-    return rootNodes;
-  }
-
   const finalInstructions = cleanData(instructions);
-
   return (
     <PageLayout title="Instructions">
       <Accordion type="multiple">
