c2939d78238962c258e45d0c47b8f2d9ca8d0e87
DmitryZaika <dmitryzaika9@gmail.com>
2025-01-09 19:48:48 -0500

    added admin.users

---
+++ app/routes/admin.users.add.tsx
@@ -0,0 +1,165 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  ActionFunctionArgs,
+  LoaderFunctionArgs,
+  redirect,
+} from "@remix-run/node";
+import { Form, useNavigate } from "@remix-run/react";
+import { FormProvider, FormField } from "../components/ui/form";
+import { getValidatedFormData } from "remix-hook-form";
+import { z } from "zod";
+import { InputItem } from "~/components/molecules/InputItem";
+import { Button } from "~/components/ui/button";
+import { useForm } from "react-hook-form";
+import {
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "~/components/ui/dialog";
+import { db } from "~/db.server";
+import { commitSession, getSession } from "~/sessions";
+import { toastData } from "~/utils/toastHelpers";
+import { useAuthenticityToken } from "remix-utils/csrf/react";
+
+import { csrf } from "~/utils/csrf.server";
+import { getAdminUser } from "~/utils/session.server";
+import { useFullSubmit } from "~/hooks/useFullSubmit";
+
+const userschema = z.object({
+  name: z.string().min(1, "Name is required"),
+  email: z.union([z.string().email(), z.literal("")]),
+  password: z.string().min(6, "Password must be at least 6 characters long"),
+  phone_number: z.union([z.coerce.string().min(10), z.literal("")]).optional(),
+  is_employee: z.union([z.coerce.string(), z.literal("")]).optional(),
+});
+
+type FormData = z.infer<typeof userschema>;
+
+const resolver = zodResolver(userschema);
+
+export async function action({ request }: ActionFunctionArgs) {
+  try {
+    await getAdminUser(request);
+  } catch (error) {
+    return redirect(`/login?error=${error}`);
+  }
+  try {
+    await csrf.validate(request);
+  } catch (error) {
+    return { error: "Invalid CSRF token" };
+  }
+
+  const { errors, data, receivedValues } = await getValidatedFormData<FormData>(
+    request,
+    resolver
+  );
+  if (errors) {
+    return { errors, receivedValues };
+  }
+  let user = getAdminUser(request);
+  try {
+    await db.execute(
+      `INSERT INTO main.users  (name, email,password, phone_number, is_employee, company_id) VALUES (?, ?, ?, ?, 1, ?)`,
+      [
+        data.name,
+        data.email,
+        data.password,
+        data.phone_number ?? null,
+        (await user).company_id,
+      ]
+    );
+  } catch (error) {
+    console.error("Error connecting to the database: ", error);
+  }
+  const session = await getSession(request.headers.get("Cookie"));
+  session.flash("message", toastData("Success", "User added"));
+  return redirect("..", {
+    headers: { "Set-Cookie": await commitSession(session) },
+  });
+}
+
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+  try {
+    const user = await getAdminUser(request);
+    return { user };
+  } catch (error) {
+    return redirect(`/login?error=${error}`);
+  }
+};
+
+export default function UsersAdd() {
+  const navigate = useNavigate();
+  const token = useAuthenticityToken();
+  const form = useForm<FormData>({
+    resolver,
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+      phone_number: "",
+    },
+  });
+  const fullSubmit = useFullSubmit(form);
+
+  const handleChange = (open: boolean) => {
+    if (open === false) {
+      navigate("..");
+    }
+  };
+
+  return (
+    <Dialog open={true} onOpenChange={handleChange}>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Add User</DialogTitle>
+        </DialogHeader>
+        <FormProvider {...form}>
+          <Form id="customerForm" method="post" onSubmit={fullSubmit}>
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <InputItem name={"Name"} placeholder={"Name"} field={field} />
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <InputItem name={"Email"} placeholder={"Email"} field={field} />
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="phone_number"
+              render={({ field }) => (
+                <InputItem
+                  name={"Phone Number"}
+                  placeholder={"Phone Number"}
+                  field={field}
+                />
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <InputItem
+                  name={"Password"}
+                  placeholder={"Password"}
+                  field={field}
+                />
+              )}
+            />
+
+            <DialogFooter>
+              <Button type="submit">Add user</Button>
+            </DialogFooter>
+          </Form>
+        </FormProvider>
+      </DialogContent>
+    </Dialog>
+  );
+}
