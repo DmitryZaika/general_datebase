0e2d2662098ebb6f014e17a519bee22dfea36b1b
DmitryZaika <dmitryzaika9@gmail.com>
2025-01-10 20:52:08 -0500

    adjusted module list

--- app/components/ModuleList.tsx
+++ app/components/ModuleList.tsx
@@ -7,7 +7,7 @@ export default function ModuleList({ children, className }: ModuleListProps) {
   return (
     <div
       className={`
-      grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 xl:grid-cols-8 2xl:grid-cols-10
+      grid grid-cols-2 sm:grid-cols-3 md:grid-cols-5 xl:grid-cols-8 2xl:grid-cols-10
       gap-2 p-2 select-none
    
       ${className}
