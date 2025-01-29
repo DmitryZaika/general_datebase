6a60f524baa86c5d7507c49e0e32779c0f7973fd
DmitryZaika <dmitryzaika9@gmail.com>
2025-01-28 20:33:49 -0500

    fixed image

--- app/components/ui/dialog.tsx
+++ app/components/ui/dialog.tsx
@@ -1,8 +1,9 @@
+// Dialog.tsx
 import * as React from "react";
 import * as DialogPrimitive from "@radix-ui/react-dialog";
 import { Cross2Icon } from "@radix-ui/react-icons";
 
-import { cn } from "~/lib/utils";
+import { cn } from "~/lib/utils"; // Убедитесь, что этот путь корректен
 
 const Dialog = DialogPrimitive.Root;
 
@@ -19,7 +20,7 @@ const DialogOverlay = React.forwardRef<
   <DialogPrimitive.Overlay
     ref={ref}
     className={cn(
-      "fixed inset-0 z-50 bg-black/80  data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0",
+      "fixed inset-0 z-50 bg-black/80 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0",
       className
     )}
     {...props}
@@ -42,9 +43,9 @@ const DialogContent = React.forwardRef<
       )}
       {...props}
     >
-      <DialogPrimitive.Close className="absolute -top-10 -right-10 text-white bg-transparent opacity-70 transition-opacity hover:opacity-100 focus:outline-none disabled:pointer-events-none ">
+      <DialogPrimitive.Close className="absolute -top-10 -right-10 text-white bg-transparent opacity-70 transition-opacity hover:opacity-100 focus:outline-none disabled:pointer-events-none">
         <Cross2Icon className="w-6 h-6" />
-        <span className="sr-only">Close</span>
+        <span className="sr-only">Закрыть</span>
       </DialogPrimitive.Close>
 
       {children}
@@ -52,6 +53,28 @@ const DialogContent = React.forwardRef<
   </DialogPortal>
 ));
 DialogContent.displayName = DialogPrimitive.Content.displayName;
+
+// Новый компонент DialogContentImage без отступов
+const DialogContentImage = React.forwardRef<
+  React.ElementRef<typeof DialogPrimitive.Content>,
+  React.ComponentPropsWithoutRef<typeof DialogPrimitive.Content>
+>(({ className, children, ...props }, ref) => (
+  <DialogPortal>
+    <DialogOverlay />
+
+    <DialogPrimitive.Content
+      ref={ref}
+      className={cn(
+        "fixed left-[50%] top-[50%] z-50 grid w-full max-w-4xl translate-x-[-50%] translate-y-[-50%] gap-4 bg-transparent p-0 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] sm:rounded-lg dark:border-zinc-800 dark:bg-zinc-950",
+        className
+      )}
+      {...props}
+    >
+      {children}
+    </DialogPrimitive.Content>
+  </DialogPortal>
+));
+DialogContentImage.displayName = "DialogContentImage";
 
 const DialogHeader = ({
   className,
@@ -115,6 +138,7 @@ export {
   DialogTrigger,
   DialogClose,
   DialogContent,
+  DialogContentImage, // Экспортируем новый компонент
   DialogHeader,
   DialogFooter,
   DialogTitle,
