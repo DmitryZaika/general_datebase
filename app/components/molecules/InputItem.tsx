29bb6a2fe4efc29dc32f7606621ed8e530ee695b
DmitryZaika <dmitryzaika9@gmail.com>
2025-02-05 19:13:07 -0500

    fixed todoList

--- app/components/molecules/InputItem.tsx
+++ app/components/molecules/InputItem.tsx
@@ -1,33 +1,34 @@
+import React, { forwardRef } from "react";
 import { FormControl, FormItem, FormLabel, FormMessage } from "../ui/form";
 import { Input } from "../ui/input";
 
-export function InputItem({
-  name,
-  placeholder,
-  field,
-  type,
-  className,
-  formClassName,
-}: {
+interface InputItemProps {
   name?: string;
   placeholder?: string;
   field: object;
   type?: string;
   className?: string;
   formClassName?: string;
-}) {
-  return (
-    <FormItem className={formClassName}>
-      {name && <FormLabel>{name}</FormLabel>}
-      <FormControl>
-        <Input
-          className={className}
-          type={type}
-          placeholder={placeholder}
-          {...field}
-        />
-      </FormControl>
-      <FormMessage />
-    </FormItem>
-  );
 }
+
+export const InputItem = forwardRef<HTMLInputElement, InputItemProps>(
+  ({ name, placeholder, field, type, className, formClassName }, inputRef) => {
+    return (
+      <FormItem className={formClassName}>
+        {name && <FormLabel>{name}</FormLabel>}
+        <FormControl>
+          <Input
+            ref={inputRef}
+            className={className}
+            type={type}
+            placeholder={placeholder}
+            {...field}
+          />
+        </FormControl>
+        <FormMessage />
+      </FormItem>
+    );
+  }
+);
+
+InputItem.displayName = "InputItem";
