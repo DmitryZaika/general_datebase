dda7917bce7d82ac2a5a091fa8e5975e13fbb0d8
DmitryZaika <dmitryzaika9@gmail.com>
2025-02-09 13:20:37 -0500

    fixed accordion-up

--- app/components/organisms/Chat.tsx
+++ app/components/organisms/Chat.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useRef, useState } from "react";
 import { DONE_KEY } from "~/utils/constants";
 import { Button } from "../ui/button";
 import { Input } from "../ui/input";
+import clsx from "clsx";
 
 interface Message {
   role: "user" | "assistant";
@@ -98,7 +99,7 @@ export const Chat = () => {
 
   return (
     <>
-      {!isOpen ? (
+      {!isOpen && (
         <button
           onClick={() => setIsOpen(true)}
           className="fixed bottom-5 right-5 cursor-pointer bg-blue-500 text-white p-4 rounded-full shadow-lg hover:bg-blue-600 transition"
@@ -118,58 +119,58 @@ export const Chat = () => {
             />
           </svg>
         </button>
-      ) : (
-        <div
-          className={`fixed bottom-0 right-0 h-[100%] w-[100%] md:w-[40%] md:h-full bg-white border-l border-gray-300 shadow-lg transform transition-transform duration-300 flex flex-col ${
-            isOpen ? "translate-x-0" : "translate-x-full"
-          }`}
-        >
-          <div className="bg-gray-100 text-gray-800 font-semibold text-lg py-3 px-4 border border-gray-300 flex justify-between items-center">
-            <span>Chat</span>
-            <Button
-              onClick={() => setIsOpen(false)}
-              variant="ghost"
-              aria-label="Close"
-              size="icon"
-              className="text-2xl"
-            >
-              ✕
-            </Button>
-          </div>
-
-          <ChatMessages
-            messages={[
-              ...messages,
-              ...(answer
-                ? [{ role: "assistant" as const, content: answer }]
-                : []),
-            ]}
-            isThinking={isThinking && !answer}
-          />
-
-          <form
-            onSubmit={handleFormSubmit}
-            className="p-4 bg-gray-100 border-t border-gray-300 flex items-center gap-2"
-          >
-            <Input
-              name="query"
-              value={input}
-              onChange={(event) => setInput(event.target.value)}
-              placeholder="Type your message..."
-              className="rounded-full"
-              autoFocus={true}
-            />
-            <Button
-              disabled={input.length === 0 || isThinking}
-              variant="blue"
-              type="submit"
-              className="rounded-full"
-            >
-              Send
-            </Button>
-          </form>
-        </div>
       )}
+      <div
+        className={clsx(
+          `fixed bottom-0 right-0 h-[100%] w-[100%] md:w-[40%] md:h-full bg-white border-l border-gray-300 shadow-lg transform transition-transform duration-300 flex flex-col animate-in fade-in animate-out fade-out`,
+          { hidden: !isOpen }
+        )}
+      >
+        <div className="bg-gray-100 text-gray-800 font-semibold text-lg py-3 px-4 border border-gray-300 flex justify-between items-center  ">
+          <span>Chat</span>
+          <Button
+            onClick={() => setIsOpen(false)}
+            variant="ghost"
+            aria-label="Close"
+            size="icon"
+            className="text-2xl"
+          >
+            ✕
+          </Button>
+        </div>
+
+        <ChatMessages
+          messages={[
+            ...messages,
+            ...(answer
+              ? [{ role: "assistant" as const, content: answer }]
+              : []),
+          ]}
+          isThinking={isThinking && !answer}
+        />
+
+        <form
+          onSubmit={handleFormSubmit}
+          className="p-4 bg-gray-100 border-t border-gray-300 flex items-center gap-2"
+        >
+          <Input
+            name="query"
+            value={input}
+            onChange={(event) => setInput(event.target.value)}
+            placeholder="Type your message..."
+            className="rounded-full"
+            autoFocus={true}
+          />
+          <Button
+            disabled={input.length === 0 || isThinking}
+            variant="blue"
+            type="submit"
+            className="rounded-full"
+          >
+            Send
+          </Button>
+        </form>
+      </div>
     </>
   );
 };
