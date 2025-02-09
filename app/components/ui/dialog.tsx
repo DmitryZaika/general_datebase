cd1cc69bf139b470658e6eb6f4fe607abb7a4640
DmitryZaika <dmitryzaika9@gmail.com>
2025-02-09 14:36:34 -0500

    fixed

--- app/components/ui/dialog.tsx
+++ app/components/ui/dialog.tsx
@@ -35,7 +35,7 @@ const dialogVariants = cva(
     variants: {
       position: {
         default:
-          "left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%] data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%]",
+          "left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%] data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:fade-out [state=open]:fade-in",
         br: "right-0 bottom-0  data-[state=closed]:slide-out-to-right data-[state=open]:slide-in-from-right",
       },
     },
