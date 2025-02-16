dc3429cdee7950290f3a642b9d04cb20d8eea3c1
DmitryZaika <dmitryzaika9@gmail.com>
2025-02-16 11:17:22 -0500

    fixed bugs

--- app/components/molecules/DialogFullHeader.tsx
+++ app/components/molecules/DialogFullHeader.tsx
@@ -1,30 +1,20 @@
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-  DialogClose,
-  DialogTrigger,
-} from "~/components/ui/dialog";
+import { DialogHeader, DialogTitle, DialogClose } from "~/components/ui/dialog";
 import { Button } from "../ui/button";
 
 export const DialogFullHeader = ({ children }: { children: JSX.Element }) => {
   return (
-    <DialogHeader>
-      <div className="text-gray-800 font-semibold text-lg py-3 px-4 border border-gray-300 flex justify-between items-center">
-        <DialogTitle>{children}</DialogTitle>
-        <DialogClose>
-          <Button
-            variant="ghost"
-            aria-label="Close"
-            size="icon"
-            className="text-2xl"
-          >
-            ✕
-          </Button>
-        </DialogClose>
-      </div>
-    </DialogHeader>
+    <div className="text-gray-800 font-semibold text-lg py-3 px-4 border border-gray-300 flex justify-between items-center">
+      <DialogTitle>{children}</DialogTitle>
+      <DialogClose>
+        <Button
+          variant="ghost"
+          aria-label="Close"
+          size="icon"
+          className="text-2xl"
+        >
+          ✕
+        </Button>
+      </DialogClose>
+    </div>
   );
 };
--- app/components/molecules/Image.tsx
+++ app/components/molecules/Image.tsx
@@ -38,7 +38,7 @@ export function Image({
 
   return (
     <div className="flex gap-2 flex-col items-center">
-      <Dialog open={isOpen} onOpenChange={handleOpenChange}>
+      <Dialog open={isOpen} onOpenChange={handleOpenChange} modal={false}>
         <DialogTrigger asChild>
           <img
             src={src || "/path/to/placeholder.png"}
