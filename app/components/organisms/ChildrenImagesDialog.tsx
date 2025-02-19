f737af1f20ac2ea33412332951640eccc0d9c2b6
DmitryZaika <dmitryzaika9@gmail.com>
2025-02-18 21:01:10 -0500

    adjusted

--- app/components/organisms/ChildrenImagesDialog.tsx
+++ app/components/organisms/ChildrenImagesDialog.tsx
@@ -22,6 +22,11 @@ export function ChildrenImagesDialog({
   id,
   setImage,
 }: ImageProps) {
+  const [data, setData] = useState<
+    { images: { id: number; url: string }[] } | undefined
+  >();
+  const [selectedImage, setSelectedImage] = useState<string | null>(src);
+
   const handleOpenChange = (open: boolean) => {
     if (!open) {
       setImage(undefined);
@@ -32,22 +37,18 @@ export function ChildrenImagesDialog({
     setImage(undefined);
   };
 
-  const [data, setData] = useState<
-    { images: { id: number; url: string }[] } | undefined
-  >();
-  const [hoveredImage, setHoveredImage] = useState<string | null>(null);
+  useEffect(() => {
+    if (isOpen) {
+      getImages();
+      setSelectedImage(src);
+    }
+  }, [isOpen, src]);
 
-  const getTodos = () => {
+  const getImages = () => {
     fetch(`/api/installed_stones/${id}`)
       .then(async (res) => await res.json())
       .then(setData);
   };
-
-  useEffect(() => {
-    if (isOpen) {
-      getTodos();
-    }
-  }, [isOpen]);
 
   return (
     <div className="flex gap-2 flex-col items-center">
@@ -58,11 +59,14 @@ export function ChildrenImagesDialog({
             alt={alt || name || "Image"}
             className={`object-cover w-40 h-40 border-2 border-blue-500 rounded cursor-pointer transition duration-200 ease-in-out transform hover:scale-[105%] hover:shadow-lg select-none hover:border-blue-500 hover:bg-gray-300 ${className}`}
             loading="lazy"
+            onAuxClick={(e) => {
+              if (e.button === 1) window.open(src || "", "_blank");
+            }}
             onClick={() => setImage(id)}
           />
         </DialogTrigger>
 
-        <DialogContent className="flex max-w-4xl flex-col justify-between items-center w-full bg-transparent h-full p-4">
+        <DialogContent className="flex flex-col justify-between items-center bg-transparent h-screen p-4">
           <button
             onClick={handleClose}
             className="absolute top-4 right-4 text-white bg-black bg-opacity-50 rounded-full p-2 hover:bg-opacity-75 transition"
@@ -73,25 +77,22 @@ export function ChildrenImagesDialog({
 
           <div className="flex-1 flex items-center justify-center w-full">
             <img
-              src={hoveredImage || src || "/path/to/placeholder.png"}
+              src={selectedImage || "/path/to/placeholder.png"}
               alt={alt || name || "Image"}
-              className="w-auto max-h-[90vh] max-w-[90vw] object-contain"
+              className="w-auto max-h-[85vh] max-w-[100vw] object-contain"
               onClick={(e) => e.stopPropagation()}
             />
           </div>
 
-          <div
-            className="flex justify-center w-full gap-2"
-            onMouseLeave={() => setHoveredImage(null)}
-          >
+          <div className="flex justify-center overflow-x-auto overflow-y-hidden md:overflow-hidden w-screen gap-1">
             {data?.images.map((image) => (
               <img
                 key={image.id}
                 src={image.url}
-                className="size-10 cursor-pointer"
+                className="w-10 h-10 cursor-pointer"
                 alt={name || "Image"}
-                onMouseEnter={() => setHoveredImage(image.url)}
-                onClick={() => setHoveredImage(image.url)}
+                onClick={() => setSelectedImage(image.url)}
+                onMouseEnter={() => setSelectedImage(image.url)}
               />
             ))}
           </div>
