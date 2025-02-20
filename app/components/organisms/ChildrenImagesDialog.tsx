788c68b6881638bba147df21890ec63bba92f87c
DmitryZaika <dmitryzaika9@gmail.com>
2025-02-19 19:29:16 -0500

    added cursor-pointer

--- app/components/organisms/ChildrenImagesDialog.tsx
+++ app/components/organisms/ChildrenImagesDialog.tsx
@@ -74,10 +74,10 @@ export function ChildrenImagesDialog({
         <DialogContent className="flex flex-col justify-between items-center bg-transparent h-screen p-4">
           <button
             onClick={handleClose}
-            className="absolute top-4 right-4 text-white bg-black bg-opacity-50 rounded-full p-2 hover:bg-opacity-75 transition"
+            className="absolute top-4 right-4 cursor-pointer text-white bg-black bg-opacity-50 rounded-full p-2 hover:bg-opacity-75 transition"
             aria-label="Close"
           >
-            <X className="w-6 h-6" />
+            <X className="w-6 h-6 " />
           </button>
 
           <div className="flex-1 flex items-center justify-center w-full">
