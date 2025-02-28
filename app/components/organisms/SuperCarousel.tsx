e992a2e83105631d5143c89b38cfab9e4ddb0c02
DmitryZaika <dmitryzaika9@gmail.com>
2025-02-28 18:37:47 -0500

    dima

--- app/components/organisms/SuperCarousel.tsx
+++ app/components/organisms/SuperCarousel.tsx
@@ -39,11 +39,12 @@ function ChildrenImagesDialog({
   };
 
   useEffect(() => {
+    console.log("HERE");
     if (isOpen) {
       getImages();
       setSelectedImage(src);
     }
-  }, [isOpen, src]);
+  }, [isOpen, src, id]);
 
   const getImages = () => {
     fetch(`/api/installed_stones/${id}`)
@@ -61,32 +62,24 @@ function ChildrenImagesDialog({
 
   return (
     <>
-      <button
-        onClick={handleClose}
-        className="absolute top-4 right-4 cursor-pointer text-white bg-black bg-opacity-50 rounded-full p-2 hover:bg-opacity-75 transition"
-        aria-label="Close"
-      >
-        <X className="w-6 h-6 " />
-      </button>
-
       <div className="flex-1 flex items-center justify-center w-full">
         <img
           src={selectedImage || "/path/to/placeholder.png"}
           alt={alt || name || "Image"}
-          className="w-auto object-contain"
+          className="w-full h-[90vh] object-contain z-0"
           onClick={(e) => e.stopPropagation()}
         />
       </div>
 
       <div
-        className="flex justify-center overflow-x-auto overflow-y-hidden md:overflow-hidden w-screen gap-1"
+        className="flex justify-center overflow-x-auto max-w-full  overflow-y-hidden md:overflow-hidden w-screen gap-2"
         onMouseLeave={handleMouseLeaveContainer}
       >
         {data?.images.map((image) => (
           <img
             key={image.id}
             src={image.url}
-            className="w-10 h-10 cursor-pointer"
+            className="w-10 h-10  cursor-pointer"
             alt={name || "Image"}
             onClick={() => setSelectedImage(image.url)}
             onMouseEnter={() => handleMouseEnter(image.url)}
@@ -116,18 +109,26 @@ export function SuperCarousel({
   const _ = useArrowCarousel(api);
 
   useEffect(() => {
-    if (api && currentId !== undefined) {
+    if (!api) return;
+    if (currentId !== undefined) {
       const index = images.findIndex(({ id }) => id === currentId);
       api.scrollTo(index, true);
     }
-  });
+    api.on("settle", (index) => {
+      console.log("SCROLL", index);
+      // setCurrentId(images[index].id);
+    });
+  }, [api]);
   return (
     <Dialog
       open={currentId !== undefined}
       onOpenChange={(open) => !open && setCurrentId(undefined)}
     >
-      <DialogContent className="flex flex-col justify-between items-center bg-transparent h-screen w-screen p-4">
-        <Carousel className="w-full" setApi={setApi}>
+      <DialogContent
+        closeClassName="z-50"
+        className=" flex flex-col justify-between items-center bg-transparent h-screen w-screen p-4"
+      >
+        <Carousel className=" max-w-screen max-h-screen " setApi={setApi}>
           <CarouselContent>
             {images.map(({ id, url }) => (
               <CarouselItem key={id}>
