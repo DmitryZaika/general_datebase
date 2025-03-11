ec1081f91a4f347482f5b986f4dd3de49ad941e8
DmitryZaika <dmitryzaika9@gmail.com>
2025-03-11 19:47:38 -0400

    added slab_inventory

--- app/components/organisms/ImageCard.tsx
+++ app/components/organisms/ImageCard.tsx
@@ -1,4 +1,6 @@
+// app/components/organisms/ImageCard.tsx
 import { Card, CardHeader, CardTitle, CardContent } from "~/components/ui/card";
+import { Link } from "react-router";
 
 import type { JSX } from "react";
 
@@ -6,24 +8,29 @@ export function ImageCard({
   children,
   fieldList,
   title,
+  stoneId,
 }: {
   children: JSX.Element;
   fieldList?: Record<string, number | null | string>;
   title: string;
+  stoneId?: number;
 }) {
   return (
-    <Card className="w-full max-w-sm">
-      <div className=" relative">{children}</div>
+    <Card className="w-full max-w-sm hover:cursor-pointer">
+      <div className="relative">{children}</div>
+
       <CardHeader className="grid gap-1 p-[2px]">
-        <CardTitle className="text-md  text-center">{title}</CardTitle>
+        <CardTitle className="text-md text-center">{title}</CardTitle>
       </CardHeader>
-      <CardContent className="py-0 px-1 cursor-pointer text-xs">
-        {fieldList &&
-          Object.entries(fieldList).map(([key, value]) => (
-            <p key={key}>
-              {key}: {value}
-            </p>
-          ))}
+      <CardContent className="py-0 px-1 text-xs">
+        <Link to={`slabs/${stoneId}`}>
+          {fieldList &&
+            Object.entries(fieldList).map(([key, value]) => (
+              <p key={key}>
+                {key}: {value}
+              </p>
+            ))}
+        </Link>
       </CardContent>
     </Card>
   );
