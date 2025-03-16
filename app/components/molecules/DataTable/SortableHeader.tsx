ef746a824b59410d3beda5c4aa377ebb70d892ac
Colin Delahunty <colin99delahunty@gmail.com>
2025-03-16 12:17:26 -0400

    Added fixes

---
+++ app/components/molecules/DataTable/SortableHeader.tsx
@@ -0,0 +1,18 @@
+import { ArrowUpDown } from "lucide-react"
+import { Column } from "@tanstack/react-table"
+import { Button } from "~/components/ui/button"
+
+interface IProps<T> {
+  column: Column<T, unknown>;
+  title: string;
+}
+
+export const SortableHeader = <T,>({ column, title }: IProps<T>) => (
+          <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          {title}
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+)
