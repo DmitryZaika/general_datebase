3e359e8686976afa18ee455193011e530eea3b39
DmitryZaika <dmitryzaika9@gmail.com>
2025-03-23 20:09:31 -0400

    adjusted filters

--- app/components/atoms/LinkSpan.tsx
+++ app/components/atoms/LinkSpan.tsx
@@ -2,10 +2,25 @@ import { cn } from "~/lib/utils";
 
 interface IProps extends React.HTMLProps<HTMLSpanElement> {
   children: React.ReactNode;
+  isSelected?: boolean;
+  variant?: 'default' | 'blue';
 }
 
-export const LinkSpan = ({ children, className, ...props }: IProps) => (
-  <span className={cn("text-blue-500 underline cursor-pointer", className )} {...props}>
+export const LinkSpan = ({ children, className, isSelected, variant = 'default', ...props }: IProps) => (
+  <span 
+    className={cn(
+      "cursor-pointer transition-colors duration-300",
+      variant === 'default' && [
+        "text-zinc-900 dark:text-zinc-50",
+        "hover:text-blue-500",
+        isSelected && "text-blue-500 hover:text-blue-500"
+      ],
+      variant === 'blue' && "text-blue-500 hover:text-blue-600",
+      className
+    )} 
+    aria-current={isSelected ? "page" : undefined}
+    {...props}
+  >
     {children}
   </span>
-)
+);
