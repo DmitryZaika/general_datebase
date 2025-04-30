945bff0f42db809c8615835f54bb5a9e3c0cfe23
DmitryZaika <dmitryzaika9@gmail.com>
2025-04-30 17:38:11 -0400

    added search

--- app/utils/headerNav.ts
+++ app/utils/headerNav.ts
@@ -4,6 +4,10 @@ export const getMirroredUrl = (isAdminPage: boolean, location: Location) => {
     const segments = location.pathname.split('/').filter(Boolean);
     const search = location.search || "";
     
+    if (segments.length >= 2 && segments[0] === "customer" && segments[2] === "stones") {
+      return `/admin/stones${search}`;
+    }
+    
     if (segments.length < 1) return isAdminPage ? `/employee${search}` : `/admin${search}`;
     
     const currentRole = segments[0]; 
@@ -23,5 +27,10 @@ export const getMirroredUrl = (isAdminPage: boolean, location: Location) => {
 
 export const getCustomerUrl = (isCustomerPage: boolean, location: Location, companyId: number | string = 1) => {
   const search = location.search || '';
+  
+  if (!isCustomerPage && location.pathname.startsWith("/admin/stones")) {
+    return `/customer/${companyId}/stones${search}`;
+  }
+  
   return isCustomerPage ? `/employee/stones${search}` : `/customer/${companyId}/stones${search}`;
 };
