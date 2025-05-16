0035efcdecee042b56ba411864b7e198f886ecdc
Colin Delahunty <colin99delahunty@gmail.com>
2025-05-16 19:17:51 -0400

    Cleaned up

---
+++ migrations/57_alter_company.sql
@@ -0,0 +1,4 @@
+ALTER TABLE company
+    ADD COLUMN qbo_client_id BLOB,
+    ADD COLUMN qbo_client_secret BLOB,
+    ADD COLUMN qbo_realm_id BLOB;
--- migrations/58_alter_company.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE company
-    ADD COLUMN qbo_client_id BLOB;
-    ADD COLUMN qbo_client_secret BLOB;
-    ADD COLUMN qbo_realm_id BLOB;
