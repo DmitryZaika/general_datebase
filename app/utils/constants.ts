0610c6aa0ff9d292746972c691f5e568260f04a1
DmitryZaika <dmitryzaika9@gmail.com>
2025-06-10 14:24:38 -0400

    Resolved merge conflict in constants.ts

--- app/utils/constants.ts
+++ app/utils/constants.ts
@@ -1,4 +1,5 @@
 import { Buffer } from "buffer";
+import { asPDFName } from "pdf-lib";
 
 export const DONE_KEY = "[DONE]035d8eba-9f8c-44c5-a1e0-290d1da033f7[/DONE]";
 
@@ -19,3 +20,46 @@ export const SINK_TYPES = [
 ] as const;
 
 export const FAUCET_TYPES = ["single handle", "double handle"] as const;
+
+export const BASE_PRICES = {
+  mitered_edge_price: 200,
+  corbels_price: 100,
+  edge_price: {
+    flat: 0,
+    eased: 100,
+    "1/4_bevel": 200,
+    "1/2_bevel": 200,
+    ogee: (linearFeet: number) => linearFeet * 25,
+    bull_nose: (linearFeet: number) => linearFeet * 18,
+  },
+  seam_price: {
+    phantom_seam: 250,
+  },
+  waterfall_price: 400,
+  "stone_t/o": (sqft: number) => sqft * 10,
+  "laminate_t/o": (sqft: number) => {
+    if (sqft < 40) return 200;
+    if (sqft >= 40 && sqft < 55) return 250;
+    if (sqft >= 55 && sqft < 70) return 300;
+    return 350;
+  },
+  stove_price: {
+    "f/s": 0,
+    "s/i": 0,
+    "c/t": 200,
+    grill: 200,
+    "n/a": 0,
+  },
+};
+
+export const CUSTOMER_ITEMS = {
+  tripFee: { miles: "number", price: (miles: number) => miles * 4 },
+  oversize_piece_handling_price: {
+    prices: {
+      "20+ sqft": 200,
+      "40+ sqft": 400,
+      "50+ sqft": 500,
+    },
+    price: (prices: number) => prices,
+  },
+};
