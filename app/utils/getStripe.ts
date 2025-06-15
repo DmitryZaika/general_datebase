3a33d869664b6dfe672b62e9495ec3524884ffbb
DmitryZaika <dmitryzaika9@gmail.com>
2025-06-15 11:26:23 -0400

    asd

--- app/utils/getStripe.ts
+++ app/utils/getStripe.ts
@@ -1,14 +1,13 @@
 import Stripe from "stripe";
 
-if (!process.env.STRIPE_SECRET_KEY) {
-    throw new Error("STRIPE_SECRET_KEY is not set in environment variables");
-}
-
 // We can safely assert the type here because we've checked it exists
-const stripeSecretKey = process.env.STRIPE_SECRET_KEY as string;
 
 export const getStripe = () => {
-    return new Stripe(stripeSecretKey, {
-        apiVersion: "2025-05-28.basil",
-    });
+  const stripeSecretKey = process.env.STRIPE_SECRET_KEY as string;
+  if (!process.env.STRIPE_SECRET_KEY) {
+    throw new Error("STRIPE_SECRET_KEY is not set in environment variables");
+  }
+  return new Stripe(stripeSecretKey, {
+    apiVersion: "2025-05-28.basil",
+  });
 };
