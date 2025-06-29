fac10837cbd4a054ef992fdcdad7bc72849cec86
DmitryZaika <dmitryzaika9@gmail.com>
2025-06-29 11:40:16 -0400

    asd

--- app/routes/admin.documents.tsx
+++ app/routes/admin.documents.tsx
@@ -1,55 +1,52 @@
-import { ColumnDef } from "@tanstack/react-table";
+import type { ColumnDef } from '@tanstack/react-table'
+import { Plus } from 'lucide-react'
+import { useEffect, useState } from 'react'
 import {
-  Table,
-  TableBody,
-  TableCaption,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "~/components/ui/table";
-import { LoaderFunctionArgs, redirect } from "react-router";
-import { selectMany } from "~/utils/queryHelpers";
-import { db } from "~/db.server";
-import { useLoaderData, Outlet, Link, useNavigation } from "react-router";
-import { Button } from "~/components/ui/button";
-import { getAdminUser } from "~/utils/session.server";
-import { ActionDropdown } from "~/components/molecules/DataTable/ActionDropdown";
-import { DataTable } from "~/components/ui/data-table";
-import { LoadingButton } from "~/components/molecules/LoadingButton";
-import { useEffect, useState } from "react";
-import { Plus } from "lucide-react";
+  Link,
+  type LoaderFunctionArgs,
+  Outlet,
+  redirect,
+  useLoaderData,
+  useNavigation,
+} from 'react-router'
+import { ActionDropdown } from '~/components/molecules/DataTable/ActionDropdown'
+import { LoadingButton } from '~/components/molecules/LoadingButton'
+import { DataTable } from '~/components/ui/data-table'
+
+import { db } from '~/db.server'
+import { selectMany } from '~/utils/queryHelpers'
+import { getAdminUser } from '~/utils/session.server'
 
 interface Document {
-  id: number;
-  name: string;
+  id: number
+  name: string
 }
 
 export const loader = async ({ request }: LoaderFunctionArgs) => {
   try {
-    await getAdminUser(request);
+    await getAdminUser(request)
   } catch (error) {
-    return redirect(`/login?error=${error}`);
+    return redirect(`/login?error=${error}`)
   }
 
-  const user = await getAdminUser(request);
+  const user = await getAdminUser(request)
   const documents = await selectMany<Document>(
     db,
-    "SELECT id, name FROM documents WHERE company_id = ?",
+    'SELECT id, name FROM documents WHERE company_id = ?',
     [user.company_id],
-  );
-  return { documents };
-};
+  )
+  return { documents }
+}
 
 const documentColumns: ColumnDef<Document>[] = [
   {
-    accessorKey: "name",
-    header: "Document Name",
+    accessorKey: 'name',
+    header: 'Document Name',
   },
   {
-    id: "actions",
+    id: 'actions',
     meta: {
-      className: "w-[50px]"
+      className: 'w-[50px]',
     },
     cell: ({ row }) => {
       return (
@@ -59,36 +56,36 @@ const documentColumns: ColumnDef<Document>[] = [
             delete: `delete/${row.original.id}`,
           }}
         />
-      );
+      )
     },
   },
-];
+]
 
 export default function Documents() {
-  const { documents } = useLoaderData<typeof loader>();
-  const navigation = useNavigation();
-  const [isAddingDocument, setIsAddingDocument] = useState(false);
+  const { documents } = useLoaderData<typeof loader>()
+  const navigation = useNavigation()
+  const [isAddingDocument, setIsAddingDocument] = useState(false)
 
   useEffect(() => {
-    if (navigation.state === "idle") {
-      if (isAddingDocument) setIsAddingDocument(false);
+    if (navigation.state === 'idle') {
+      if (isAddingDocument) setIsAddingDocument(false)
     }
-  }, [navigation.state]);
+  }, [navigation.state])
 
   const handleAddDocumentClick = () => {
-    setIsAddingDocument(true);
-  };
+    setIsAddingDocument(true)
+  }
 
   return (
-    <div >
-      <Link to={`add`} relative="path" onClick={handleAddDocumentClick}>
+    <div>
+      <Link to={`add`} relative='path' onClick={handleAddDocumentClick}>
         <LoadingButton loading={isAddingDocument}>
-          <Plus className="w-4 h-4 mr-1" />
+          <Plus className='w-4 h-4 mr-1' />
           Add Document
         </LoadingButton>
       </Link>
       <DataTable columns={documentColumns} data={documents} />
       <Outlet />
     </div>
-  );
+  )
 }
