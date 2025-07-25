d6dc264e667b7cf369a9f5fb0b3cda0e6a9d70e2
Dima <dmitryzaika9@gmail.com>
2025-07-25 01:00:18 +0000

    fxied bugs

--- app/routes/employee.customers.add.tsx
+++ app/routes/employee.customers.add.tsx
@@ -1,77 +1,14 @@
-import { zodResolver } from '@hookform/resolvers/zod'
-import { useForm } from 'react-hook-form'
+import { useMutation } from '@tanstack/react-query'
 import {
-  type ActionFunctionArgs,
-  Form,
   type LoaderFunctionArgs,
   redirect,
+  useLoaderData,
   useNavigate,
 } from 'react-router'
-import { getValidatedFormData } from 'remix-hook-form'
-import { useAuthenticityToken } from 'remix-utils/csrf/react'
-import { z } from 'zod'
-import { InputItem } from '~/components/molecules/InputItem'
-import { Button } from '~/components/ui/button'
-import {
-  Dialog,
-  DialogContent,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-} from '~/components/ui/dialog'
-import { db } from '~/db.server'
-import { useFullSubmit } from '~/hooks/useFullSubmit'
-import { commitSession, getSession } from '~/sessions'
-import { csrf } from '~/utils/csrf.server'
+import { CustomerForm } from '~/components/pages/CustomerForm'
+import { useToast } from '~/hooks/use-toast'
+import { type CustomerDialogSchema, createCustomerMutation } from '~/schemas/customers'
 import { getEmployeeUser } from '~/utils/session.server'
-import { toastData } from '~/utils/toastHelpers'
-import { FormField, FormProvider } from '../components/ui/form'
-
-const customerSchema = z.object({
-  name: z.string().min(1),
-  email: z.string().email().optional(),
-  phone: z.union([z.coerce.string().min(10), z.literal('')]),
-  address: z.string().min(10).optional(),
-})
-
-type FormData = z.infer<typeof customerSchema>
-
-const resolver = zodResolver(customerSchema)
-
-export async function action({ request }: ActionFunctionArgs) {
-  let user: { company_id: number; id: number }
-  try {
-    user = await getEmployeeUser(request)
-  } catch (error) {
-    return redirect(`/login?error=${error}`)
-  }
-  try {
-    await csrf.validate(request)
-  } catch (error) {
-    return { error: 'Invalid CSRF token' }
-  }
-
-  const { errors, data } = await getValidatedFormData<FormData>(request, resolver)
-  if (errors) {
-    return { errors }
-  }
-
-  try {
-    await db.execute(
-      `INSERT INTO customers (name, email, phone, address, company_id ) VALUES (?,?,?,?,?)`,
-      [data.name, data.email, data.phone, data.address, user.company_id],
-    )
-  } catch (error) {
-    console.error('Error connecting to the database: ', error)
-    return { error: 'Database error' }
-  }
-
-  const session = await getSession(request.headers.get('Cookie'))
-  session.flash('message', toastData('Success', 'Customer added'))
-  return redirect('..', {
-    headers: { 'Set-Cookie': await commitSession(session) },
-  })
-}
 
 export const loader = async ({ request }: LoaderFunctionArgs) => {
   try {
@@ -83,76 +20,32 @@ export const loader = async ({ request }: LoaderFunctionArgs) => {
 }
 
 export default function CustomersAdd() {
+  const toast = useToast()
+  const { user } = useLoaderData<typeof loader>()
   const navigate = useNavigate()
-  const token = useAuthenticityToken()
-  const form = useForm<FormData>({
-    resolver,
-  })
-  const fullSubmit = useFullSubmit(form)
+
+  const onSuccess = () => {
+    console.log('onSuccess')
+    navigate('..')
+  }
+
+  const mutation = useMutation(createCustomerMutation(toast, onSuccess))
+
   const handleChange = (open: boolean) => {
     if (open === false) {
       navigate('..')
     }
   }
 
+  const onSubmit = (data: CustomerDialogSchema) => {
+    mutation.mutate({ ...data, company_id: user.company_id })
+  }
+
   return (
-    <Dialog open={true} onOpenChange={handleChange}>
-      <DialogContent className='sm:max-w-[425px]'>
-        <DialogHeader>
-          <DialogTitle>Add Customer</DialogTitle>
-        </DialogHeader>
-        <FormProvider {...form}>
-          <Form id='customerForm' method='post' onSubmit={fullSubmit}>
-            <FormField
-              control={form.control}
-              name='name'
-              render={({ field }) => (
-                <InputItem
-                  name={'Name'}
-                  placeholder={'Name of the customer'}
-                  field={field}
-                />
-              )}
-            />
-            <FormField
-              control={form.control}
-              name='email'
-              render={({ field }) => (
-                <InputItem
-                  name={'Email'}
-                  placeholder={"Customer's email"}
-                  field={field}
-                />
-              )}
-            />
-            <FormField
-              control={form.control}
-              name='phone'
-              render={({ field }) => (
-                <InputItem
-                  name={'Phone'}
-                  placeholder={"Customer's phone number"}
-                  field={field}
-                />
-              )}
-            />
-            <FormField
-              control={form.control}
-              name='address'
-              render={({ field }) => (
-                <InputItem
-                  name={'Address'}
-                  placeholder={"Customer's address"}
-                  field={field}
-                />
-              )}
-            />
-            <DialogFooter>
-              <Button type='submit'>Save changes</Button>
-            </DialogFooter>
-          </Form>
-        </FormProvider>
-      </DialogContent>
-    </Dialog>
+    <CustomerForm
+      handleChange={handleChange}
+      onSubmit={onSubmit}
+      isLoading={mutation.isPending}
+    />
   )
 }
