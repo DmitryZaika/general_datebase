7f1d556a66b115d04c8fa6aef2204a9a75c2cb4c
Dima <dmitryzaika9@gmail.com>
2025-08-18 19:29:22 +0000

    Add new columns to customers table in migration script for backsplash, kitchen stove, message, and attached file

---
+++ migrations/90_alter_customers.sql
@@ -0,0 +1,5 @@
+ALTER TABLE customers
+  ADD COLUMN backsplash VARCHAR(255) NULL,
+  ADD COLUMN kitchen_stove VARCHAR(255) NULL,
+  ADD COLUMN your_message TEXT NULL,
+  ADD COLUMN attached_file VARCHAR(255) NULL;
