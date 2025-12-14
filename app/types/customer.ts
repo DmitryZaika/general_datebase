bd8c5ebc5c585831c670621d64186016d3dd3590
Colin Delahunty <colin99delahunty@gmail.com>
2025-12-14 13:45:34 -0500

    Handle more emails

---
+++ app/types/customer.ts
@@ -0,0 +1,12 @@
+export type Sources = 'check-in' | 'check-list' | 'leads' | 'call-in' | 'other'
+
+export interface Customer {
+  id: number
+  name: string
+  address: string | null
+  phone: string | null
+  email: string | null
+  company_name: string | null
+  source: Sources | null
+  your_message: string | null
+}
