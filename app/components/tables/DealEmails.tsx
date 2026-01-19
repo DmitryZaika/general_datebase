332fbe75b951ba957440a78ddd84ae186819046f
Colin Delahunty <colin99delahunty@gmail.com>
2026-01-19 10:56:46 -0500

    show date again

--- app/components/tables/DealEmails.tsx
+++ app/components/tables/DealEmails.tsx
@@ -2,20 +2,29 @@ import type { ColumnDef, Row } from '@tanstack/react-table'
 import { Eye, EyeClosed } from 'lucide-react'
 import type { EmailHistory } from '~/crud/emails'
 
+function cleanDate(value: string): string {
+  const date = new Date(value)
+  return date.toLocaleDateString('en-US', {
+    month: 'short',
+    day: 'numeric',
+  })
+}
+
 const DateCell = ({ row }: { row: Row<EmailHistory> }) => {
-  if (row.original.read_count === 0) {
-    return <EyeClosed />
-  }
+  if (row.original.read_count === 0) return <EyeClosed />
   return <Eye className='text-blue-500' />
 }
 
 export const customerColumns: ColumnDef<EmailHistory>[] = [
-  { accessorKey: 'sent_at', header: 'Date', cell: DateCell },
+  { accessorKey: 'read', header: 'Read', cell: DateCell },
+  {
+    accessorKey: 'sent_at',
+    header: 'Date',
+    cell: ({ row }) => cleanDate(row.original.sent_at),
+  },
   {
     accessorKey: 'subject',
     header: 'Subject',
-    cell: ({ row }: { row: Row<EmailHistory> }) => {
-      return row.original.subject
-    },
+    cell: ({ row }) => row.original.subject,
   },
 ]
