b4952687a1c8c3e529813d2d63bee73efbe5e917
Colin Delahunty <colin99delahunty@gmail.com>
2026-01-25 13:31:25 -0500

    Fixed conflict

---
+++ app/crud/sinks.ts
@@ -0,0 +1,24 @@
+import { db } from '~/db.server'
+import type { SaleSink } from '~/types/sales'
+import { selectMany } from '~/utils/queryHelpers'
+
+export async function getSlabInventorySinks(saleId: number) {
+  return await selectMany<SaleSink>(
+    db,
+    `SELECT
+      sinks.id,
+      sinks.sink_type_id,
+      sink_type.name,
+      sinks.price,
+      sinks.is_deleted,
+      sinks.slab_id,
+      slab_inventory.room,
+      HEX(slab_inventory.room_uuid) as room_uuid
+     FROM sinks
+     JOIN sink_type ON sinks.sink_type_id = sink_type.id
+     JOIN slab_inventory ON sinks.slab_id = slab_inventory.id
+     WHERE slab_inventory.sale_id = ? AND sinks.is_deleted = 0
+     ORDER BY sinks.id`,
+    [saleId],
+  )
+}
