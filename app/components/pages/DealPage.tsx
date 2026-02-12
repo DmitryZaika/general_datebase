ec083609d3a93082d4cca0a715e0fdc55123e865
Dima <dmitryzaika9@gmail.com>
2026-02-12 23:30:32 +0000

    style: refine layout and improve component consistency
    - Adjusted padding and margin in DealPage and DataTable components for better visual alignment and responsiveness.
    - Updated the DealActivityPanel to streamline icon imports and enhance overall component structure.
    - Renamed a project field in CustomerInfoTab for clarity, improving user understanding of the displayed information.

--- app/components/pages/DealPage.tsx
+++ app/components/pages/DealPage.tsx
@@ -38,9 +38,9 @@ export default function DealsEdit({
 
   return (
     <Dialog open={true} onOpenChange={handleChange}>
-      <DialogContent className='sm:max-w-[1100px] overflow-auto md:overflow-hidden flex flex-col justify-baseline h-auto min-h-[600px] max-h-[95vh] md:h-[85vh] p-5'>
+      <DialogContent className='sm:max-w-[1100px] overflow-auto md:overflow-hidden flex flex-col justify-baseline h-auto min-h-[600px] max-h-[95vh] md:h-[85vh] py-4 px-1 sm:p-5  '>
         <DialogHeader>
-          <DialogTitle>Edit Deal</DialogTitle>
+          <DialogTitle className='px-1 sm:px-2'>Edit Deal</DialogTitle>
         </DialogHeader>
         {showProgress && (
           <div className='mb-2'>
@@ -51,8 +51,8 @@ export default function DealsEdit({
             />
           </div>
         )}
-        <div className='grid grid-cols-1 md:grid-cols-[1.75fr_2fr] flex-1 min-h-0 overflow-auto md:overflow-hidden'>
-          <div className='pl-2 pr-2 md:pr-4 md:overflow-auto md:min-h-0'>
+        <div className='grid grid-cols-1 md:grid-cols-[2fr_2fr] flex-1 min-h-0 overflow-auto md:overflow-hidden'>
+          <div className='px-1 sm:px-2  md:overflow-auto md:min-h-0'>
             <Tabs
               value={location.pathname.split('/').pop()}
               onValueChange={value => navigate(`${value}${location.search}`)}
@@ -67,7 +67,7 @@ export default function DealsEdit({
               <Outlet />
             </Tabs>
           </div>
-          <div className='border-t md:border-t-0 md:border-l pt-4 md:pt-0 pl-2 md:pl-4 pr-2 md:overflow-auto md:min-h-0'>
+          <div className='border-t md:border-t-0 md:border-l pt-4 md:pt-0 px-1 sm:px-2 pr-2 md:overflow-auto md:min-h-0'>
             <DealActivityPanel dealId={dealId} activities={activities} />
           </div>
         </div>
